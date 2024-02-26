7e8d2df98475e250a9e8c74fd86b83b4a5edf329
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2024-02-26 19:40:00 +0700

    App icon added Launch screen set up Row height setup

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -32,10 +32,16 @@ class MainViewController: UITableViewController {
         var content = cell.defaultContentConfiguration()
         
         content.text = restaurantName
-//        content.image
+        content.image = UIImage(named: restaurantNames[indexPath.row])
+        content.imageProperties.cornerRadius = cell.frame.size.height / 2
         cell.contentConfiguration = content
         
         return cell
+    }
+    
+    //    MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
     
     /*
