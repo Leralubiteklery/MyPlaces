1cd4a0947f8f5ab381c7718688962d1a245693ab
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2024-04-05 14:58:21 +0300

    Added Map Kit View screen.

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Lera Savchenko on 5.04.24.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closeVC(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
