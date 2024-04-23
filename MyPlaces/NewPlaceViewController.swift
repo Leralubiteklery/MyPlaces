2ca86a1864c6c5d4b749da8b4d83f4ed9bb32b2d
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2024-04-23 21:34:34 +0300

    Passed addres to NewPlaceVC.

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -81,6 +81,7 @@ class NewPlaceViewController: UITableViewController {
         else { return }
         
         mapVC.incomeSegueIdentifier = identifier
+        mapVC.mapViewControllerDelegate = self
         
         if identifier == "showPlace" {
             mapVC.place.name = placeName.text!
@@ -188,5 +189,12 @@ extension NewPlaceViewController:UIImagePickerControllerDelegate, UINavigationCo
         imageIsChanged = true
         
         dismiss(animated: true)
+    }
+}
+
+extension NewPlaceViewController: MapViewControllerDelegate {
+    
+    func getAddress(_ address: String?) {
+        placeLocation.text = address
     }
 }
